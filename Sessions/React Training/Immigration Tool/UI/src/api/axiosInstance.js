06e7b32dd4529134de02fa0503d87ae8822a71b5
SathishIdeas2it <sathishkumar.rajagopalan@ideas2it.com>
2024-11-27 19:44:42 +0530

    Base version of UI added

---
+++ Sessions/React Training/Immigration Tool/UI/src/api/axiosInstance.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:80';
+const token = localStorage.getItem('token');
+
+const axiosInstance = axios.create({
+  baseURL: baseURL, 
+  timeout: 10000,                  
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error('Unauthorized, redirecting to login...');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
